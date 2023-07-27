cfdb7d12f5e8ee927c11e1099b1ca57c8d69aa9d
Pablo Silva <pmarcelosilvag@gmail.com>
2023-07-27 14:39:22 -0400

    build:database

--- src/utils/database.js
+++ src/utils/database.js
@@ -8,16 +8,15 @@ const db = new Sequelize({
     username: config.db.user,
     password: config.db.pass,
     database: config.db.name,
-    port: config.db.port,
-    dialectOptions:
-        process.env.NODE_ENV === 'production' ? {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false
-            }
-        } : {}
+    port: config.db.port
+    // dialectOptions:
+    //     process.env.NODE_ENV === 'production' ? {
+    //         ssl: {
+    //             require: true,
+    //             rejectUnauthorized: false
+    //         }
+    //     } : {}
 
 })
-
 
 module.exports = db
